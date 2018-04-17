9ba32a35af107c5f2f459d697c01daeaec5bba9f
Henrik Feldt <henrik@haf.se>
2018-04-17 18:42:42 +0200

    initial

---
+++ Web/Program.fs
@@ -0,0 +1,22 @@
+﻿open Hopac
+open System
+open Logary
+open Logary.Configuration
+open Logary.Internals
+open Logary.Ingestion
+open Suave
+open Suave.Filters
+open Suave.Operators
+
+let app next =
+  choose [
+    path "/i" >=> HTTP.api HTTP.defaultConfig next
+    path "/o" >=> HTTP.api HTTP.defaultConfig next
+    RequestErrors.NOT_FOUND "Not found"
+  ]
+
+[<EntryPoint>]
+let main argv =
+  let next
+  startWebServer defaultConfig app
+  0
